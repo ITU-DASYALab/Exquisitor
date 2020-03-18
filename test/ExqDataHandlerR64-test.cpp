1bfd990514d413a49f27a8a842bd9b9d67ff2a9c
Omar Shahbaz Khan <ok2610@gmail.com>
2020-03-18 13:55:34 +0100

    Added success message for tests

--- test/ExqDataHandlerR64-test.cpp
+++ test/ExqDataHandlerR64-test.cpp
@@ -11,6 +11,8 @@
 using namespace exq;
 using std::vector;
 using std::string;
+using std::cout;
+using std::endl;
 
 class SingleModalityDataHandlerFixture: public ::testing::Test {
 public:
@@ -40,6 +42,8 @@ TEST_F(SingleModalityDataHandlerFixture, firstItemRead) {
     ASSERT_EQ(firstItem->getTop(), 2533274790396757);
     ASSERT_EQ(firstItem->getFeatureIds(), 1125912792203265);
     ASSERT_EQ(firstItem->getFeatureRatios(), 242353128979235512);
+
+    cout << "TEST firstItemRead in SingleModalityDataHandlerFixture SUCCEEDED!" << endl;
 }
 
 TEST_F(SingleModalityDataHandlerFixture, lastitemRead) {
@@ -49,8 +53,12 @@ TEST_F(SingleModalityDataHandlerFixture, lastitemRead) {
     ASSERT_EQ(lastItem->getTop(), 281474976711517);
     ASSERT_EQ(lastItem->getFeatureIds(), 1407404948324356);
     ASSERT_EQ(lastItem->getFeatureRatios(), 255864967252411121);
+
+    cout << "TEST lastItemRead in SingleModalityDataHandlerFixture SUCCEEDED!" << endl;
 }
 
 TEST_F(SingleModalityDataHandlerFixture, count) {
     ASSERT_EQ(dataHandler->getItems()->at(0)->size(), 5);
+
+    cout << "TEST count in SingleModalityDataHandlerFixture SUCCEEDED!" << endl;
 }
