da576302f19dab0a74723f88db70cd9c2def5b3f
Omar Shahbaz Khan <ok2610@gmail.com>
2020-05-11 17:07:43 +0200

    Added class for Python

---
+++ src/ExqPython.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Omar Shahbaz Khan on 11/05/2020.
+//
+
+#include "ExqPython.h"
---
+++ src/ExqPython.h
@@ -0,0 +1,16 @@
+//
+// Created by Omar Shahbaz Khan on 11/05/2020.
+//
+
+#ifndef EXQUISITOR_EXQPYTHON_H
+#define EXQUISITOR_EXQPYTHON_H
+
+
+
+class ExqPython {
+
+};
+
+
+
+#endif //EXQUISITOR_EXQPYTHON_H
