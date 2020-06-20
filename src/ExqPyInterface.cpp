ea80df35a1dae5c2f674db28f5244f71a7f22e7d
Omar Shahbaz Khan <ok2610@gmail.com>
2020-06-20 14:21:35 +0200

    Rename ExqPython to ExqPyInterface

--- src/ExqPyInterface.cpp
+++ src/ExqPyInterface.cpp
@@ -2,4 +2,4 @@
 // Created by Omar Shahbaz Khan on 11/05/2020.
 //
 
-#include "ExqPython.h"
+#include "ExqPyInterface.h"
---
+++ src/ExqPyInterface.h
@@ -0,0 +1,16 @@
+//
+// Created by Omar Shahbaz Khan on 11/05/2020.
+//
+
+#ifndef EXQUISITOR_EXQPYINTERFACE_H
+#define EXQUISITOR_EXQPYINTERFACE_H
+
+namespace exq {
+
+    class ExqPyInterface {
+
+    };
+
+}
+
+#endif //EXQUISITOR_EXQPYINTERFACE_H
