6fe4060a2525cb2926dad62640f16efa75a13770
Omar Shahbaz Khan <ok2610@gmail.com>
2020-07-30 12:42:10 +0200

    Added Python3 inludes/libraries. Python3 interface method signatures implemented

--- src/ExqPyInterface.cpp
+++ src/ExqPyInterface.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "ExqPyInterface.h"
+
+using namespace exq;
+
+PyObject* initialize_py(PyObject* self, PyObject* args) {
+
+}
+
+PyObject* train_py(PyObject* self, PyObject* args) {
+
+}
+
+PyObject* suggest_py(PyObject* self, PyObject* args) {
+
+}
+
+PyObject* reset_model_py(PyObject* self, PyObject* args) {
+
+}
+
+PyObject* safe_exit_py(PyObject* self, PyObject* args) {
+
+}
+
+PyMODINIT_FUNC PyInit_exquisitor(void) {
+    Py_Initialize();
+    return PyModule_Create(&exquisitor_definition);
+}
--- src/ExqPyInterface.h
+++ src/ExqPyInterface.h
@@ -5,10 +5,32 @@
 #ifndef EXQUISITOR_EXQPYINTERFACE_H
 #define EXQUISITOR_EXQPYINTERFACE_H
 
+#include <Python.h>
+
 namespace exq {
 
-    class ExqPyInterface {
+    PyMODINIT_FUNC PyInit_exquisitor(void);
 
+    static PyObject* initialize_py(PyObject* self, PyObject* args);
+    static PyObject* train_py(PyObject* self, PyObject* args);
+    static PyObject* suggest_py(PyObject* self, PyObject* args);
+    static PyObject* reset_model_py(PyObject* self, PyObject* args);
+    static PyObject* safe_exit_py(PyObject* self, PyObject* args);
+
+    static PyMethodDef exquisitor_methods[] = {
+            {"initialize", initialize_py, METH_VARARGS, "Initialize exquisitor"},
+            {"train", train_py, METH_VARARGS, "Train the classifier (linear SVM)"},
+            {"suggest", suggest_py, METH_VARARGS, "Get suggestions from current model"},
+            {"reset_model", reset_model_py, METH_VARARGS, "Reset the model"},
+            {"safe_close", safe_exit_py, METH_NOARGS, "Safely free up memory"}
+    };
+
+    static struct PyModuleDef exquisitor_definition = {
+            PyModuleDef_HEAD_INIT,
+            "exq",
+            "A Python module that executes Exquisitor functions.",
+            -1,
+            exquisitor_methods
     };
 
 }
