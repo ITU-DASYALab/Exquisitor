8461f7d80a4f4a439c00028e03dcfd5e77e29904
Omar Shahbaz Khan <ok2610@gmail.com>
2021-09-27 14:04:47 +0200

    Add ECPPriorityQueue class. Unimplemented.

---
+++ src/base/eCP/ECPPriorityQueue.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Omar Shahbaz Khan on 27/09/2021.
+//
+
+#include "ECPPriorityQueue.h"
---
+++ src/base/eCP/ECPPriorityQueue.h
@@ -0,0 +1,14 @@
+//
+// Created by Omar Shahbaz Khan on 27/09/2021.
+//
+
+#ifndef EXQUISITOR_ECPPRIORITYQUEUE_H
+#define EXQUISITOR_ECPPRIORITYQUEUE_H
+
+
+class ECPPriorityQueue {
+
+};
+
+
+#endif //EXQUISITOR_ECPPRIORITYQUEUE_H
