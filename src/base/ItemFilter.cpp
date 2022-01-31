e4f9c41004b60fc9781aac51615aee912f6a667d
Omar Shahbaz Khan <ok2610@gmail.com>
2022-01-31 15:10:02 +0100

    Initial files for filter comparison.

---
+++ src/base/ItemFilter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Omar Shahbaz Khan on 13/12/2021.
+//
+
+#include "ItemFilter.h"
---
+++ src/base/ItemFilter.h
@@ -0,0 +1,41 @@
+//
+// Created by Omar Shahbaz Khan on 13/12/2021.
+//
+
+#ifndef EXQUISITOR_ITEMFILTER_H
+#define EXQUISITOR_ITEMFILTER_H
+
+#include "Metadata/ItemProperties.h"
+
+namespace exq {
+    class ItemFilter {
+    public:
+        virtual bool compare() = 0;
+    };
+
+    class LSC_ItemFilter : public ItemFilter {
+    public:
+        bool compare() override;
+
+    private:
+        int day;
+        int hour;
+        int location;
+    };
+
+    class VBS_ItemFilter : public ItemFilter {
+    public:
+        bool compare() override;
+
+    private:
+        int video;
+        int faces;
+        int color;
+        int text;
+        ExqArray<int> ctgs;
+        ExqArray<int> tags;
+    };
+}
+
+
+#endif //EXQUISITOR_ITEMFILTER_H
