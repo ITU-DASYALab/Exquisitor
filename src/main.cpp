7972c0490dac038d48fff591bef1d3a025fecbb6
ok2610 <ok2610@gmail.com>
2023-05-09 14:16:16 +0000

    Add inlcudes in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#include "base/r64/ExqDataHandlerECP.h"
+#include "base/r64/ExqFunctionsR64.h"
+#include "base/ExqController.h"
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
